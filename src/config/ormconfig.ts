1895b8a72dce898670fd36b7145ebf7896a10397
Filip <f.ilioski@heisterkamp.eu>
2020-02-04 13:13:27 +0100

    fixing db connection

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -3,7 +3,7 @@ import { ConnectionOptions } from 'typeorm';
 export const config: ConnectionOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: Number(process.env.DBPORT),
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
